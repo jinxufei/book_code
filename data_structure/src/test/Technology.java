bb5e51628d690d864f8e1c412794b3dc4c683c76
jinxufei <978954852@qq.com>
2016-12-28 11:38:47 +0800

    111

---
+++ data_structure/src/test/Technology.java
@@ -0,0 +1,14 @@
+package data_structure.src.test;
+
+/**
+ * Created by jinxufei on 2016/12/12.
+ */
+public enum Technology {
+    Mybatis,
+    SpingMVC,
+    Maven,
+    MySQL,
+    Shiro,
+    Redis,
+    SSH
+}
--- data_structure/src/test/Test.java
+++ data_structure/src/test/Test.java
@@ -5,5 +5,14 @@ package data_structure.src.test;
  */
 
 public class Test {
+    public static void main(String[] args) {
+        String technology[] = {"敏捷管理","Mybatis/iBatis","SpingMVC","Maven","MySQL","shiro","Redis","SSH","Idea","git",
+        "jekins","JIRA","并发","other"};
 
+        /*if (学历≧本科&&工作经验≧3&&你的技能==technology){
+            System.out.println("加入掌众共创大数据平台");
+        }*/
+
+
+    }
 }
